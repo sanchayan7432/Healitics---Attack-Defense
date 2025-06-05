60a87b1530e435cd3a086cadc5ffde993d591bac
Sanchayan Ghosh <113206213+sanchayan7432@users.noreply.github.com>
2025-06-05 21:38:28 +0530

    Update model_factory.py

--- model_factory.py
+++ model_factory.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 
 def get_model():
-    genai.configure(api_key="AIzaSyDDKvpe02p6R-fYL0lCUDNBLWJRiBR4B78")
+    genai.configure(api_key="...")
     return genai.GenerativeModel(
         model_name="gemini-2.0-flash",
         system_instruction=(
